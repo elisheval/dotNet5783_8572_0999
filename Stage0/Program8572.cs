bd43074f4edca99da4bf4e644978cc41efc69dda
elisheval <elisheva2272@gmail.com>
2022-09-16 16:28:37 +0300

    i started the project...

---
+++ Stage0/Program8572.cs
@@ -0,0 +1,25 @@
+﻿//// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using System;
+
+namespace Stage0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome8572();
+            Welcome0999();
+            Console.ReadKey();
+        }
+         static partial void Welcome0999();
+
+        private static void Welcome8572()
+        {
+            Console.Write("enter your name ");
+            string userName = Console.ReadLine();
+            Console.WriteLine(userName + " ,welcome to my first console application");
+        }
+    }
+}
