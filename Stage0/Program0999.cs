42e9c952d181bf309aefa717cd48afa00b89d01f
elisheva shrem <elish41844@gmail.com>
2022-10-01 20:43:45 +0300

    welcome0999  function

---
+++ Stage0/Program0999.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stage0
+{
+	partial class Program
+	{
+		static partial void Welcome0999()
+		{
+			string name;
+			Console.Write("what is your name?");
+			name = Console.ReadLine();
+			Console.WriteLine($"Your name is{name}");
+		}
+	}
+}
