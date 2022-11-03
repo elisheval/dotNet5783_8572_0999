5fcec643ff4db482cb852820009ae788a6b96a18
elisheva shrem <elish41844@gmail.com>
2022-11-03 21:31:36 +0200

    בניית שכבת הנתונים והוספת מתודות במחלקות גישה של הישויות

--- DalFacade/DO/Product.cs
+++ DalFacade/DO/Product.cs
@@ -4,12 +4,20 @@ namespace DO;
 
 public struct Product
 {
-    public int Id { get; set; }
+
+    public int Id { get; set; } = -1;
     public string Name { get; set; }
     public double Price { get; set; }
     public Category Category { get; set; }
     public int InStock { get; set; }
 
+     Product(string name, double price, Category category, int inStock)
+     {
+        Name = name;
+        Price= price;
+        Category = category;
+        InStock = inStock;
+     }
     public override string ToString() {
         return ($@"
            Product ID={Id}: {Name}, 
--- DalList/DalProduct.cs
+++ DalList/DalProduct.cs
@@ -1,6 +1,57 @@
 ﻿
+using DO;
+
 namespace Dal;
 
 public struct DalProduct
 {
+    public int Add(Product myProduct)
+    {
+        myProduct.Id= DataSource.Config.IdentifyProduct;
+        DataSource.productArr[DataSource.Config.IndexProduct++] = myProduct;
+        return myProduct.Id;
+    }
+    public Product Get(int myId)
+    {
+        for(int i = 0; i < DataSource.Config.IndexProduct; i++)
+        {
+            if (DataSource.productArr[i].Id==myId)
+                return DataSource.productArr[i];
+        }
+        throw new Exception("no product found with this ID");
+    }
+
+
+    public Product[] GetAll()
+    {
+        return DataSource.productArr;
+    }
+
+    public void Delete(int myId)
+    {
+        for (int i = 0; i < DataSource.Config.IndexProduct; i++)
+        {
+            if (DataSource.productArr[i].Id == myId)
+            {
+                DataSource.productArr[i] = DataSource.productArr[--DataSource.Config.IndexProduct];
+                return;
+            }
+
+        }
+        throw new Exception("no product found to delete with this ID");
+
+    }
+    public void Update(Product myProduct)
+    {
+        for (int i = 0; i < DataSource.Config.IndexProduct; i++)
+        {
+            if (DataSource.productArr[i].Id == myProduct.Id)
+            {
+                DataSource.productArr[i] = myProduct;
+                return;
+            }
+        }
+        throw new Exception("no product found to update with this ID");
+
+    }
 }
