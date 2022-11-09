48a5285b6491a99837b86b4b3c0ec0e490c30537
elisheva shrem <elish41844@gmail.com>
2022-11-09 22:32:29 +0200

    date

--- DalFacade/DO/Order.cs
+++ DalFacade/DO/Order.cs
@@ -8,15 +8,16 @@ public struct Order
     public string CustomerName { get; set; }
     public string CustomerEmail { get; set; }
     public string CustomerAddress { get; set; }
-    public DateTime OrderDate { get; set; }
+    public DateTime OrderDate { get; set; } = DateTime.MinValue;
     public DateTime ShipDate { get; set; }
     public DateTime DeliveryDate { get; set; }
 
-    Order(string customerName, string customerEmail, string customerAddress, DateTime orderDate, DateTime shipDate, DateTime deliveryDate)
-    {  //DateTime.MinValue מה זה timespan
+    Order(string customerName, string customerEmail, string customerAddress, DateTime shipDate, DateTime deliveryDate, DateTime orderDate)// = DateTime.MinValue)
+    {
+        //DateTime.MinValue מה זה timespan
         CustomerName = customerName;
         CustomerEmail = customerEmail;
-        CustomerAddress = customerAddress;
+        CustomerAddress = customerAddress;     
         OrderDate = orderDate;
         ShipDate = shipDate;
         DeliveryDate = deliveryDate;
